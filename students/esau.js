7884b477b5777fbeee803a4e7cfe61b7b63ef0c6
Esaú JC <esau.jc@gmail.com>
2018-09-24 12:51:37 +0200

    Two hidde button-sections improved with Text Button

--- students/esau.js
+++ students/esau.js
@@ -8,11 +8,22 @@ var sectionS1 = document.querySelector('section.perfil');
 function handleClickS1(e){
   //e.classList.toggle('hidden');
   sectionS1.classList.toggle('hidden');
+  if (sectionS1.classList.contains('hidden')){
+    e.currentTarget.innerText = 'Show Perfil';
+  }else{
+    e.currentTarget.innerText = 'Hide Perfil';
+  }
 }
 
 function handleClickS2(e){
   //e.classList.toggle('hidden');
   sectionS2.classList.toggle('hidden');
+  if (sectionS2.classList.contains('hidden')){
+    e.currentTarget.innerText = 'Show Experiments';
+  }else{
+    e.currentTarget.innerText = ' Hide Experiments';
+  }
+
 }
 
 buttonS1.addEventListener('click',handleClickS1);
