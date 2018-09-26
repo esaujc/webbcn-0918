d8712f513c2d1c9b30f98cdf15fce7f727d2b981
Esaú JC <esau.jc@gmail.com>
2018-09-26 22:47:32 +0200

    Add new design, some responsive, some flex. Not finished

--- students/esau.js
+++ students/esau.js
@@ -32,6 +32,144 @@ function main(){
   buttonS1.addEventListener('click',handleClickS1);
   buttonS2.addEventListener('click',handleClickS2);
 
+// Search Part
+  
+function findStudents(searchTerm){
+  var results = [];
+  if (searchTerm){
+    results = students.filter(function(student){
+    var studentName = student.name.toLowerCase();
+      if (studentName.indexOf(searchTerm) !== -1){
+        return true;
+      }
+    })
+  }
+   return results;
+}
+
+function displayResults(results){
+  searchResultsElement = document.querySelector('.search-result');
+  searchResultsElement.innerHTML = '';
+  var searchResultsListElement = document.createElement('ul');
+
+  results.forEach(function(result){
+    var resultLink = document.createElement('a');
+    resultLink.innerText = result.name;
+    resultLink.setAttribute('href','../'+result.url);
+
+    var resultListItem = document.createElement('li');
+    resultListItem.appendChild(resultLink);
+
+    searchResultsListElement.appendChild(resultListItem);
+  })
+  searchResultsElement.appendChild(searchResultsListElement);
+
+}
+
+
+
+function handleKeyUp(){
+
+  var searchTerm = input.value.toLowerCase();
+  var results = findStudents(searchTerm);
+ 
+  displayResults(results);
+  
+  
+
+}
+   
+
+var input = document.querySelector('.input-student input');
+input.addEventListener('keyup', handleKeyUp);
+var searchResultsElement ='';
+
+// Esto es para que al pulsar Esc, elimine la lista y mantenga el texto del input
+// Al ponerlo fuera, controla escape desde cualquier parte.
+document.body.addEventListener('keyup',function(event){
+  if (event.key === 'Escape'){
+    searchResultsElement.innerHTML = '';
+    return;
+  }
+});
+
+document.body.addEventListener('click',function(event){
+ // console.log(event);
+//  if (event.target !== input){ // Esto hace que cuando llegue el click propagado del input, no lo tenga en cuenta
+  //if (event.key === 'click'){
+//    searchResultsElement.innerHTML = ''; //Esta línea da error ahora...
+    return;
+  //}
+// }
+});
+
+input.addEventListener('click',function(event){
+  event.stopPropagation();
+})
+
+
+// AQUI LA PARTE DEL TIEMPO
+var divInterval = document.querySelector('.setInterval');
+
+var buttonInit = document.querySelector('.initInterval');
+buttonInit.addEventListener('click',handleClickInit);
+var divID = 0;
+
+function handleClickInit(){
+buttonInit.setAttribute("disabled", "disabled");
+buttonStopInterval.removeAttribute("disabled");
+buttonStopInterval.hidden = false;
+
+divInterval.innerText = '';
+var timeLeft = 5;
+
+divID = setInterval(function(){
+  divInterval.innerText = timeLeft;
+  
+  if(timeLeft <= 0){
+    clearInterval(divID);
+    divInterval.innerText = 'BOOOOMM!!!';
+    buttonInit.removeAttribute("disabled");
+    buttonStopInterval.setAttribute("disabled", "disabled");
+    buttonStopInterval.hidden = true;
+
+  }
+  timeLeft--;
+},1000)
+
+//console.log(divID);
+
+return divID;
+}
+
+// var buttonRandom = 1;
+// buttonRandom = Math.round(Math.random())*(2-1)+1;
+// console.log("Random: " + buttonRandom);
+// var clasePrueba = '.stopInterval'+buttonRandom;
+// var buttonStopInterval = document.querySelector('.stopInterval'+ buttonRandom);
+
+var buttonStopInterval = document.querySelector('.stopInterval1');
+buttonStopInterval.addEventListener('click',handleStopInterval);
+buttonStopInterval.setAttribute("disabled", "disabled");
+buttonStopInterval.hidden = true;
+
+function handleStopInterval(){
+if (divID){
+  clearInterval(divID);
+  buttonInit.removeAttribute("disabled");
+  buttonStopInterval.setAttribute("disabled", "disabled");
+  buttonStopInterval.hidden = true;
+  divInterval.innerText = '';
+  
+}
+console.log("Paró");
+
+}
+
+
+
+
+
 }
 
 window.addEventListener('load',main);
